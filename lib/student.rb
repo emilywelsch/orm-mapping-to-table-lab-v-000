dd6b07f29fc17f63afde565f976b7d45b0f73ccf
Emily Welsch <emwelsch@gmail.com>
2018-07-26 15:10:25 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,17 @@
 class Student
+  # access your database connection anywhere in this class with DB[:conn]
+  attr_accessor :name, :grade
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  def self.create_table
+  end
+
+  def self.drop_table
+  end
+
+  def save
+  end
+
+  def self.create
+  end
+
 end
